bfbd26b5a686a69182d6355d65b9bc0061a2ff0b
Kevin Andrade <130163890+kaandradec@users.noreply.github.com>
2025-05-12 18:10:59 -0500

    taller 12: Hola Mundo con Vue.JS

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <HolaMundoVue></HolaMundoVue>
+</template>
+
+<script>
+import HolaMundoVue from "./views/HolaMundoView.vue";
+
+export default {
+  name: "App",
+  components: {
+    HolaMundoVue,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
